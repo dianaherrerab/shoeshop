5e0801bcbf9b2067c1e207cd748e0ac8a863a07a
dianaherrerab <aleja-herrera98@hotmail.com>
2021-01-20 12:07:24 -0500

    beta

--- App/Resources/Templates/footer.php
+++ App/Resources/Templates/footer.php
@@ -1,5 +1,38 @@
+<footer class="py-5">
+    <div class="container">
+        <div class="row justify-content-md-between">
+            <div class="col-12 col-md-7 pb-5 pb-md-0">
+                <h4 class="font-weight-bold m-0 p-0 white-text text-center text-md-left mb-4 mb-md-0">ACERCA DE NOSOTROS</h4>
+                <hr class="d-none d-md-block">
+                <p class="white-text text-center text-md-left m-0 p-0">
+                    ShoeShop es un proyecto que busca expandir y digitalizar el mercado de calzado de Bucaramanga a nivel nacional, incrementando su popularidad y generando desarrollo para el municipio.
+                </p>
+            </div>
+            <div class=" col-12 col-md-4 pb-5 pb-md-0 pl-md-5 pl-0">
+                <h4 class="font-weight-bold m-0 p-0 white-text text-center text-md-left mb-4 mb-md-0">PRODUCTOS</h4>
+                <hr class="d-none d-md-block">
+                <ul class="white-text text-center text-md-left m-0 p-0">
+                    <ol class="px-0 pb-2">DAMAS</ol>
+                    <ol class="px-0 pb-2">CABALEROS</ol>
+                    <ol class="px-0 pb-2">NIÑOS</ol>
+                    <ol class="px-0">DEPORTIVOS</ol>
+                </ul>
+            </div>
+            <div class="col-12 col-md-1 p-0 d-flex flex-md-column justify-content-around align-items-md-center">
+                <a href=""><i class="fab fa-facebook fa-3x white-text"></i></a>
+                <a href=""><i class="fab fa-instagram fa-3x white-text"></i></a>
+                <a href=""><i class="fab fa-twitter-square fa-3x white-text"></i></a>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+
 
 </body>
 
-estees el footer dsfsdfsd
+
 </html>
--- App/Resources/Views/welcome.php
+++ App/Resources/Views/welcome.php
@@ -1,22 +1,9 @@
 <?php require_once RESOURCES."/Templates/header.php"; ?>
-
-	<div class="header">
-		<div class="container">
-			<div class="row">
-				
-			</div>
-		</div>
-	</div>
 	
 	<div class="container">
-		<div class="row align-items-center justify-content-center" style="height: 100vh;">
-			<div class="col-12 text-center">
-				<div class="row">
-					<div class="col-12">
-						
-					</div>
-				</div>
-			</div>
+		<div class="row" style="height: 100vh;">
+
+
 		</div>
 	</div>
 
